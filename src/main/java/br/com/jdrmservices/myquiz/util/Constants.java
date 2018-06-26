31f4caba033b8e93e87de5be4796e00e42cfb74d
JonasDiego21 <jonasdiegorodriguesmartins@yahoo.com.br>
2018-06-26 17:10:50 -0300

    Add validations messages

--- src/main/java/br/com/jdrmservices/myquiz/util/Constants.java
+++ src/main/java/br/com/jdrmservices/myquiz/util/Constants.java
@@ -4,6 +4,18 @@ public class Constants {
 	// messages
 	public static final String MESSAGE_ADD_SUCCESS = "Informações gravadas com sucesso!";
 	public static final String MESSAGE_ADD_ERROR = "Não foi possível gravar as informações";
+	public static final String MESSAGE_ALTER_SUCCESS = "Informações alteradas com sucesso!";
+	public static final String MESSAGE_ALTER_ERROR = "Não foi possível alterar as informações";
 	public static final String MESSAGE_REMOVE_SUCCESS = "Informações excluídas com sucesso!";
-	public static final String MESSAGE_REMOVE_ERROR = "Não foi possível excluir as informações";	
+	public static final String MESSAGE_REMOVE_ERROR = "Não foi possível excluir as informações";
+	
+	// validations
+	public static final String NAME_REQUIRED = "Nome é obrigatório";
+	public static final String EMAIL_REQUIRED = "E-mail é obrigatório";
+	public static final String STATEMENT_QUESTION_REQUIRED = "O enunciado da questão é obrigatório";
+	public static final String ALTERNATIVE_A = "Alternativa a é obrigatória";
+	public static final String ALTERNATIVE_B = "Alternativa a é obrigatória";
+	public static final String ALTERNATIVE_C = "Alternativa a é obrigatória";
+	public static final String ALTERNATIVE_D = "Alternativa a é obrigatória";
+	public static final String CORRECT_ALTERNATIVE = "Alternativa a é obrigatória";
 }

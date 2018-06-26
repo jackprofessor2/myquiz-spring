41cdba7a6fa53838440a84a3f17753be09e82893
JonasDiego21 <jonasdiegorodriguesmartins@yahoo.com.br>
2018-06-26 19:40:56 -0300

    Create aluno controller

--- src/main/java/br/com/jdrmservices/myquiz/controller/AlunosController.java
+++ src/main/java/br/com/jdrmservices/myquiz/controller/AlunosController.java
@@ -1,16 +1,41 @@
 package br.com.jdrmservices.myquiz.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.jdrmservices.myquiz.model.Aluno;
+import br.com.jdrmservices.myquiz.service.AlunoService;
 
 @RestController
 @RequestMapping("/alunos")
 public class AlunosController {
 
-	@GetMapping
-	public String novo() {
-		return "Cadastrar Aluno";
+	@Autowired
+	private AlunoService alunoService;
+	
+	@GetMapping(produces = "application/json")
+	public @ResponseBody Iterable<Aluno> novo() {
+		Iterable<Aluno> listAlunos = alunoService.list();
+		return listAlunos;
+	}
+	
+	@PostMapping
+	public Aluno createProfessor(@RequestBody @Valid Aluno aluno) {
+		return alunoService.save(aluno);
+	}
+	
+	@DeleteMapping
+	public Aluno removeQuiz(@RequestBody Aluno aluno) {
+		alunoService.delete(aluno);
+		return aluno;
 	}
 	
 }

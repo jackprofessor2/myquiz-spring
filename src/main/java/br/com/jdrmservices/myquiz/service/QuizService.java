7cec677bec7c5baf49616691b7811714e447e765
JonasDiego21 <jonasdiegorodriguesmartins@yahoo.com.br>
2018-06-26 18:10:27 -0300

    Create quiz service

--- src/main/java/br/com/jdrmservices/myquiz/service/QuizService.java
+++ src/main/java/br/com/jdrmservices/myquiz/service/QuizService.java
@@ -1,29 +1,42 @@
 package br.com.jdrmservices.myquiz.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.jdrmservices.myquiz.exception.NegocioException;
+import br.com.jdrmservices.myquiz.model.Quiz;
+import br.com.jdrmservices.myquiz.repository.Quizes;
+import br.com.jdrmservices.myquiz.util.Constants;
 
 @Service
 public class QuizService {
 
-public void list() {
-		
+	@Autowired
+	private Quizes quizes;
+	
+	public List<Quiz> list() {
+		return quizes.findAll();
 	}
 	
 	@Transactional
-	public void create() {
+	public void save(Quiz quiz) {
+		Optional<Quiz> quizOptional = quizes.findByEnunciadoIgnoreCase(quiz.getCodigo());
 		
+		if(quiz.isNovo() && quizOptional.isPresent()) {
+			throw new NegocioException(Constants.MESSAGE_EXISTS);
+		}
+		
+		quizes.saveAndFlush(quiz);
 	}
 	
 	@Transactional
-	public void edit() {
-		
-	}
-	
-	@Transactional
-	public void remove() {
-		
+	public void delete(Quiz quiz) {
+		quizes.delete(quiz);
 	}
 	
 }

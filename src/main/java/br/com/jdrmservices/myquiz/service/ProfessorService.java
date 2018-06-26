80c7e773c0298f84b842fa2d3fa6aa276a550350
JonasDiego21 <jonasdiegorodriguesmartins@yahoo.com.br>
2018-06-26 18:07:25 -0300

    Create professor service

--- src/main/java/br/com/jdrmservices/myquiz/service/ProfessorService.java
+++ src/main/java/br/com/jdrmservices/myquiz/service/ProfessorService.java
@@ -1,29 +1,42 @@
 package br.com.jdrmservices.myquiz.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import br.com.jdrmservices.myquiz.exception.NegocioException;
+import br.com.jdrmservices.myquiz.model.Professor;
+import br.com.jdrmservices.myquiz.repository.Professores;
+import br.com.jdrmservices.myquiz.util.Constants;
 
 @Service
 public class ProfessorService {
 
-public void list() {
-		
+	@Autowired
+	private Professores professores;
+	
+	public List<Professor> list() {
+		return professores.findAll();
 	}
 	
 	@Transactional
-	public void create() {
+	public void save(Professor professor) {
+		Optional<Professor> professorOptional = professores.findByNomeIgnoreCase(professor.getCodigo());
 		
+		if(professor.isNovo() && professorOptional.isPresent()) {
+			throw new NegocioException(Constants.MESSAGE_EXISTS);
+		}
+		
+		professores.saveAndFlush(professor);
 	}
 	
 	@Transactional
-	public void edit() {
-		
-	}
-	
-	@Transactional
-	public void remove() {
-		
+	public void delete(Professor professor) {
+		professores.delete(professor);
 	}
 	
 }

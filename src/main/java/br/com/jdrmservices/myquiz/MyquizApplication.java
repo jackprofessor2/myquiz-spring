ce9ad25c190167e3417bff7629da06c19a4c221b
JonasDiego21 <jonasdiegorodriguesmartins@yahoo.com.br>
2018-06-26 10:54:24 -0300

    Alterações de configuração

--- src/main/java/br/com/jdrmservices/myquiz/MyquizApplication.java
+++ src/main/java/br/com/jdrmservices/myquiz/MyquizApplication.java
@@ -1,9 +1,11 @@
 package br.com.jdrmservices.myquiz;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class MyquizApplication {
 
 	public static void main(String[] args) {
--- src/main/java/br/com/jdrmservices/myquiz/service/ProfessorService.java
+++ src/main/java/br/com/jdrmservices/myquiz/service/ProfessorService.java
@@ -1,8 +1,29 @@
 package br.com.jdrmservices.myquiz.service;
 
+import javax.transaction.Transactional;
+
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProfessorService {
 
+public void list() {
+		
+	}
+	
+	@Transactional
+	public void create() {
+		
+	}
+	
+	@Transactional
+	public void edit() {
+		
+	}
+	
+	@Transactional
+	public void remove() {
+		
+	}
+	
 }

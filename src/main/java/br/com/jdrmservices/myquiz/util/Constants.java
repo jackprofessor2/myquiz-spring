5c3c6977ba018f58f64e85ba94162108b92e89be
JonasDiego21 <jonasdiegorodriguesmartins@yahoo.com.br>
2018-06-26 15:02:00 -0300

    Add default messages in constants class

--- src/main/java/br/com/jdrmservices/myquiz/util/Constants.java
+++ src/main/java/br/com/jdrmservices/myquiz/util/Constants.java
@@ -1,5 +1,9 @@
 package br.com.jdrmservices.myquiz.util;
 
 public class Constants {
-
+	// messages
+	public static final String MESSAGE_ADD_SUCCESS = "Informações gravadas com sucesso!";
+	public static final String MESSAGE_ADD_ERROR = "Não foi possível gravar as informações";
+	public static final String MESSAGE_REMOVE_SUCCESS = "Informações excluídas com sucesso!";
+	public static final String MESSAGE_REMOVE_ERROR = "Não foi possível excluir as informações";	
 }
